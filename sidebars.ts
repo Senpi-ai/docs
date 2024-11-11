920f41257c7e00c0c0fecb091748ae5a4dd3d064
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2024-11-11 19:12:58 +0400

    remove developer preview

--- sidebars.ts
+++ sidebars.ts
@@ -39,7 +39,7 @@ const sidebars: SidebarsConfig = {
     },
     {
       type: "category",
-      label: "Moxie Earn Developer Preview",
+      label: "Moxie Earn",
       className: "protocol_fees",
       link: { type: "doc", id: "learn/moxie-earn/index" },
       items: [
